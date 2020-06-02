ab0b207677eb33b67746b21feea3b28617142feb
kevin-kili <kilikevin435@gmail.com>
2020-06-02 07:04:19 +0300

    added post_crate

--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import (
-    PostListView
+    PostListView,
+    PostCreateView
 )
 app_name = 'insta'
 
 urlpatterns = [
     path('',PostListView.as_view(),name ='post_list'),
+    path('new/',PostCreateView.as_view,name='post_create'),
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .forms import PostForm
 from .models import Post
-from django.views.generic import (ListView)
+from django.views.generic import (ListView,CreateView)
 
 # Create your views here.
 class PostListView(ListView):
     template_name = 'Insta/post_list.html'
     queryset = Post.objects.all()
     context_object_name = 'posts'
+
+class PostCreateView(CreateView):
+    template_name ='Insta/post_create.html'
+    from_class = PostForm
+    queryset = Post.objects.all()
+
+
+    def form_valid(self, form):
+        print(form.claned_data)
+        form.Instance.author = self.request.user
+        return super().form_valid(form)
